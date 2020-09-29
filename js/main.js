be6a6ffc1f98dc383336b984b338e02bc851bbdf
Rebecca Mead <beckyleemead@gmail.com>
2020-09-29 09:46:40 -0400

    fixing asynchronous behavior

--- js/main.js
+++ js/main.js
@@ -11,36 +11,39 @@ const weather_condition = document.getElementById("weather_condition");
 const seasonal_pic = document.getElementById("seasonal_pic");
 
 // get_weather function to run when button is pressed
-let get_weather = function (z) {
+async function get_weather(z) {
     // pass zip code input to openweather api
-    fetch('https://api.openweathermap.org/data/2.5/weather?zip='+z+'&appid=fcec01a7089ad8ad44cce9a1071a284c').then((weather_data) => {
+    let result = await fetch('https://api.openweathermap.org/data/2.5/weather?zip=' + z + '&appid=fcec01a7089ad8ad44cce9a1071a284c').then((weather_data) => {
         if (weather_data.ok) {
             return weather_data.json();
         } else {
             // throw an error if the URL with the given zip code didn't work
             return Promise.reject('error')
         };
-    }).then((weather_json) => {
-        // parse the returned data for city, temperature, condition, icon code
-        // return the data that will be passed to populate_html
-        let output = {"city": weather_json.name, "temperature": weather_json.main.temp, "condition": weather_json.weather.main, "icon_code": weather_json.icon};
-        return output;
-    }).catch((error) => {
+    }).then(weather_json => {
+        // parse the returned data and return what's needed for populate_html
+        let parsed = {
+            "city": weather_json.name,
+            "temperature": weather_json.main.temp,
+            "condition": weather_json.weather.main,
+            "icon_code": weather_json.icon
+        };
+        return parsed;
+    }).catch(error => {
         // alert user to errors
         alert('something didn\'t work there. please try again!')
     });
+    return result;
 }
 
 // populate_html function to populate index.html with weather data
-    // input = output from get_weather
-    // convert temperature into all three of k/f/c
-    // populate html elements with the relevant data
+// input = output from get_weather
+// convert temperature into all three of k/f/c
+// populate html elements with the relevant data
 
 // add event listener to "get weather" button
-input_button.addEventListener("click", function () {
-    // run get_weather as promise using fetch()
-    let weather_data = get_weather(input_zip.value); // ! doesn't appear to be working
-    console.log(weather_data);
+input_button.addEventListener("click", async function () {
+    // run get_weather
+    let weather_data = await get_weather(input_zip.value);
     // then populate_html
-    // should this have some sort of timeout handling? does that happen automatically with promises?
 })
